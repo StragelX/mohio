978f0777aa8a994955a554597a818e9eab9bb394
anton shulgin <stragelx@gmail.com>
2022-08-03 16:27:10 +0300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
+  $(".products").click(function () {
+    $(this).toggleClass("active");
+    $(".main_mav").toggleClass("active");
+    $("body").toggleClass("no_scroll");
+  });
+
   if ($(".main_top_games_carousel")) {
     $(".main_top_games_carousel.owl-carousel").owlCarousel({
       items: 4,
       loop: true,
       autoplay: true,
+    });
+  }
+
+  if ($(".more_games_carousel")) {
+    $(".more_games_carousel.owl-carousel").owlCarousel({
+      items: 4,
+      loop: true,
+      autoplay: true,
+      margin: 40,
     });
   }
 
@@ -22,7 +37,7 @@ $(document).ready(function () {
       loop: true,
       autoplay: true,
       nav: true,
-      navText: ["1", "2"],
+      navText: ["", ""],
     });
   }
 
@@ -32,6 +47,70 @@ $(document).ready(function () {
       loop: true,
       autoplay: true,
       nav: true,
+      navText: ["", ""],
     });
   }
+
+  $(".games_table .table-cell").mouseenter(function () {
+    let id = $(this).attr("data-num");
+
+    $(".games_table .table-cell").each(function () {
+      if ($(this).attr("data-num") && $(this).attr("data-num") == id) {
+        $(this).addClass("table_collumn_hover");
+      }
+    });
+  });
+
+  $(".games_table .table-cell").mouseleave(function () {
+    $(".games_table .table-cell").each(function () {
+      $(this).removeClass("table_collumn_hover");
+    });
+  });
+
+  $(".zoom-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: "mfp-with-zoom mfp-img-mobile",
+    image: {
+      verticalFit: true,
+      titleSrc: function (item) {
+        return (
+          item.el.attr("title") +
+          ' &middot; <a class="image-source-link" href="' +
+          item.el.attr("data-source") +
+          '" target="_blank">image source</a>'
+        );
+      },
+    },
+    gallery: {
+      enabled: true,
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+      opener: function (element) {
+        return element.find("img");
+      },
+    },
+  });
+
+  $(".call_modal").click(function () {
+    let modal_id = $(this).attr("data-modal");
+    $(".modal_overlay").removeClass("hidden");
+    $("body").toggleClass("no_scroll");
+    $(".modal").each(function () {
+      if ($(this).attr("data-id") == modal_id) {
+        $(this).removeClass("hidden");
+      }
+    });
+  });
+
+  $(".modal .close, .modal_overlay").click(function (e) {
+    e.stopPropagation();
+    $(".modal").addClass("hidden");
+    $("body").toggleClass("no_scroll");
+    $(".modal_overlay").addClass("hidden");
+  });
 });
