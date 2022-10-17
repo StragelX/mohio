f10f8d1970a29b432f7cb6a73b77581977f06df3
anton shulgin <stragelx@gmail.com>
2022-10-17 17:14:01 +0300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,26 @@ $(document).ready(function () {
   });
 
   $(".show_mob_nav").click(function () {
+    if ($(".products").hasClass("active")) {
+      $(".products").trigger("click");
+    }
     $(this).toggleClass("active");
     $(".mob_nav").toggleClass("active");
+    $("body, html").toggleClass("no_scroll");
   });
 
   $(".products").click(function () {
+    if ($(".show_mob_nav").hasClass("active")) {
+      $(".show_mob_nav").trigger("click");
+    }
     $(this).toggleClass("active");
     $(".main_mav").toggleClass("active");
-    $("body").toggleClass("no_scroll");
+    $("body, html").toggleClass("no_scroll");
+  });
+
+  $(".close_mob").click(function () {
+    $(".mob_nav").removeClass("active");
+    $("body, html").removeClass("no_scroll");
   });
 
   if ($(".main_top_games_carousel")) {
@@ -93,6 +105,7 @@ $(document).ready(function () {
     $(".main_distribution_carousel.owl-carousel").owlCarousel({
       loop: true,
       autoplay: true,
+      margin: 16,
 
       responsive: {
         0: {
@@ -153,9 +166,31 @@ $(document).ready(function () {
     });
   }
 
+  if ($(".footer_carousel")) {
+    $(".footer_carousel.owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+
+      responsive: {
+        0: {
+          items: 1.4,
+        },
+        770: {
+          items: 3,
+        },
+        1000: {
+          items: 4,
+        },
+        1360: {
+          items: 5,
+        },
+      },
+    });
+  }
+
   $(".show_mob_drop").click(function (e) {
     e.stopPropagation();
-    $(".show_mob_drop_list").toggleClass("hidden");
+    $(this).find(".show_mob_drop_list").toggleClass("hidden");
   });
 
   $(".games_table .table-cell").mouseenter(function () {
@@ -206,7 +241,7 @@ $(document).ready(function () {
   $(".call_modal").click(function () {
     let modal_id = $(this).attr("data-modal");
     $(".modal_overlay").removeClass("hidden");
-    $("body").toggleClass("no_scroll");
+    $("body, html").toggleClass("no_scroll");
     $(".modal").each(function () {
       if ($(this).attr("data-id") == modal_id) {
         $(this).removeClass("hidden");
@@ -217,12 +252,12 @@ $(document).ready(function () {
   $(".modal .close, .modal_overlay").click(function (e) {
     e.stopPropagation();
     $(".modal").addClass("hidden");
-    $("body").toggleClass("no_scroll");
+    $("body, html").toggleClass("no_scroll");
     $(".modal_overlay").addClass("hidden");
   });
 
   $(".language_drop").click(function () {
-    $(this).addClass("active");
+    $(this).toggleClass("active");
   });
 
   $(document).click(function (event) {
@@ -295,22 +330,126 @@ $(document).ready(function () {
     );
   });
 
-  // map data
-  simplemaps_worldmap.hooks.click_state = function (id) {
-    if (simplemaps_worldmap_mapdata.state_specific[id].desc) {
-      $(".map_data .title").html(
-        simplemaps_worldmap_mapdata.state_specific[id].name
-      );
-
-      $(".map_data .desc").html(
-        simplemaps_worldmap_mapdata.state_specific[id].desc
-      );
-    }
-  };
-
   if (window.location.hash && window.location.hash.slice(1, -1) == "team") {
     let id = window.location.hash.slice(1);
     document.getElementById(id).click();
     document.getElementById(id).scrollIntoView();
   }
+
+  // phone autocompleat regio
+  if ($(".contacts_tel").length) {
+    const contactsphoneInputField = document.querySelector(".contacts_tel");
+    const contactsphoneInput = window.intlTelInput(contactsphoneInputField, {
+      initialCountry: "auto",
+      geoIpLookup: getIp,
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+
+    $(".contacts_tel").keyup(function () {
+      setTimeout(() => {
+        $(".contacts_tel_hidden").val(contactsphoneInput.getNumber());
+      }, 200);
+    });
+  }
+
+  if ($(".footer_phone").length) {
+    const footerphoneInputField = document.querySelector(".footer_phone");
+    const footerphoneInput = window.intlTelInput(footerphoneInputField, {
+      initialCountry: "auto",
+      geoIpLookup: getIp,
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+
+    $(".footer_phone").keyup(function () {
+      setTimeout(() => {
+        $(".footer_phone_hidden").val(footerphoneInput.getNumber());
+      }, 200);
+    });
+  }
+  // end regio
+
+  // reting regio
+  // get value from local storage
+  let reting = parseInt(window.localStorage.getItem(window.location.href));
+
+  if (!isNaN(reting)) {
+    $(".star").siblings().removeClass("active");
+    let j = 0;
+    $(".star").each(function () {
+      if (j < reting) {
+        $(this).addClass("active");
+        j++;
+      }
+    });
+  }
+
+  $(".star").click(function () {
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+    $(".star.active").prevAll().addClass("active");
+
+    let i = 0;
+
+    $(".star.active").each(function () {
+      i++;
+    });
+
+    window.localStorage.setItem(window.location.href, i);
+  });
+  // end regio
+
+  // scroll to top regio
+  if ($(".scroll-to-top")) {
+    const scrollBtn = document.querySelector(".scroll-to-top-btn");
+
+    const btnVisibility = () => {
+      if (window.scrollY > 400) {
+        scrollBtn.style.visibility = "visible";
+      } else {
+        scrollBtn.style.visibility = "hidden";
+      }
+    };
+
+    document.addEventListener("scroll", () => {
+      btnVisibility();
+    });
+
+    scrollBtn.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    });
+  }
+  // endregio
+
+  // map data
+  if (simplemaps_worldmap_mapdata) {
+    simplemaps_worldmap.hooks.click_state = function (id) {
+      if (simplemaps_worldmap_mapdata.state_specific[id].desc) {
+        $(".map_data .title").html(
+          simplemaps_worldmap_mapdata.state_specific[id].name
+        );
+
+        $(".map_data .desc").html(
+          simplemaps_worldmap_mapdata.state_specific[id].desc
+        );
+      }
+    };
+  }
 });
+
+function getIp(callback) {
+  fetch("https://ipinfo.io/json?token=12c751951b8632", {
+    headers: { Accept: "application/json" },
+  })
+    .then((resp) => resp.json())
+    .catch(() => {
+      return {
+        country: "us",
+      };
+    })
+    .then((resp) => callback(resp.country));
+}
